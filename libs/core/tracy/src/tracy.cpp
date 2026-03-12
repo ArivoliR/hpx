910d4714dac321293eebf50c085952c0837baab8
ArivoliR <arivoli2005@gmail.com>
2026-03-12 05:38:33 +0530

    add per-worker-thread colors
    Signed-off-by: ArivoliR <arivoli2005@gmail.com>

--- libs/core/tracy/src/tracy.cpp
+++ libs/core/tracy/src/tracy.cpp
@@ -9,15 +9,33 @@
 #if defined(HPX_HAVE_MODULE_TRACY)
 #include <hpx/tracy/tracy.hpp>
 
+#include <atomic>
+#include <cstdint>
 #include <string>
 
 #include <tracy/Tracy.hpp>
 
 namespace hpx::tracy {
 
+    namespace {
+        constexpr std::uint32_t thread_palette[] = {
+            0xe6194b, 0x3cb44b, 0x4363d8, 0xf58231,
+            0x911eb4, 0x42d4f4, 0xf032e6, 0xbfef45,
+            0xfabed4, 0x469990, 0xdcbeff, 0x9a6324,
+        };
+        constexpr int palette_size =
+            static_cast<int>(sizeof(thread_palette) / sizeof(thread_palette[0]));
+
+        std::atomic<int> thread_counter{0};
+        thread_local int thread_color_index = -1;
+    }    // namespace
+
     void set_thread_name(char const* name) noexcept
     {
         ::tracy::SetThreadName(name);
+        if (thread_color_index < 0)
+            thread_color_index = thread_counter.fetch_add(1) % palette_size;
+        TracySetThreadColor(thread_palette[thread_color_index]);
     }
 
     namespace {
@@ -27,7 +45,6 @@ namespace hpx::tracy {
         thread_local bool fiber_active = false;
     }    // namespace
 
-    // Expose Tracy fibers support
     void enter_fiber(char const* name) noexcept
     {
         bool const connected = ::tracy::GetProfiler().IsConnected();
@@ -55,14 +72,12 @@ namespace hpx::tracy {
         ::tracy::Profiler::SendFrameMark(name, ::tracy::QueueType::FrameMarkMsgEnd);
     }
 
-    // Create a new plot in Tracy
     void create_counter(std::string const& name) noexcept
     {
         ::TracyPlotConfig(
             name.c_str(), ::tracy::PlotFormatType::Number, true, false, 0);
     }
 
-    // Pass a plot value to Tracy
     void sample_value(std::string const& name, double const value) noexcept
     {
         ::TracyPlot(name.c_str(), value);
